2546e9c0dcb6c2bd25fbed53267a5847811fbff3
Henrique S. Souza <henrique_eduardo_souza@hotmail.com>
2024-10-29 16:35:27 -0300

    feat: add API test

--- src/app/Content/Articles/page.jsx
+++ src/app/Content/Articles/page.jsx
@@ -1,12 +1,33 @@
+'use client'
+ 
+
 import React from 'react'
+
+import { useState, useEffect } from 'react'
 
 import './Articles.css';''
 
 function Articles() {
+  const [data, setData] = useState([])
+  const [dataCards, setDataCards] = useState(null)
 
+  async function handleClick() {
+    let res = await fetch('https://vigent.saude.sp.gov.br/sisaweb_api/lista.php?')
+    let apiData = await res.json()
+    setData(apiData)
+    console.log(data)
+  }
+  
   return (
     <div className="container">
-      
+      <button onClick={handleClick}>API FETCH</button>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <strong>ID:</strong> {item.id} - <strong>Nome:</strong> {item.nome}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
