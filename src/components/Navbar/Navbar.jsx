42f26180d6e134f23b7a6fef2dc37a12cdf4456f
Henrique S. Souza <henrique_eduardo_souza@hotmail.com>
2024-10-29 10:14:29 -0300

    feat: add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-
-import './Navbar.css';
+import './Navbar.css'
 
 function Navbar() {
 
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="navbar">
+      <div className="container">
+        <div className='sid-container'>
+          <a to='/'>SID</a>
+        </div>
+        <ul>
+          <li><a href='/articles'>artigos</a></li>
+          <li><a href='/data'>dados</a></li>
+          <li><a href='/contribute'>contribua</a></li>
+          <li><a href='/acess'>acesso</a></li>
+        </ul>
+        <div>
+
+        </div>
+      </div>
+    </nav>
   )
 }
 
