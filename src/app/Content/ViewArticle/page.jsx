ea5f2089cf5a7a3ea832be24b8162bdccc575c2f
CarlosEngler <Carlos.engler@usp.br>
2024-12-02 15:44:53 -0300

    remove quill from viewArticle and make buttons work

--- src/app/Content/ViewArticle/page.jsx
+++ src/app/Content/ViewArticle/page.jsx
@@ -3,84 +3,84 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState, Suspense } from 'react';
 import { apiFetch } from '../../../core/auth';
-
 import './ViewArticle.css';
-
 import Footer from '@/components/Footer/Footer';
 import Navbar from '@/components/Navbar/Navbar';
-import Quill from 'quill';
 import QuillTextEditor from './QuillTextEditor';
 
 function ViewArticle() {
     return (
-      <div className="page-container">
-        <Navbar section="articles"/>
-        <Suspense>
-          <Content/>
-        </Suspense>
-        <Footer/>
-      </div>
+        <div className="page-container">
+            <Navbar section="articles" />
+            <Suspense>
+                <Content />
+            </Suspense>
+            <Footer />
+        </div>
     );
-  }
+}
 
 function Content() {
     const searchParams = useSearchParams();
     const articleID = searchParams.get('id');
     const [articleData, setArticleData] = useState(null);
-    const router = useRouter();
 
     useEffect(() => {
         fetchData(articleID);
-    }, [])
+    }, []);
 
     async function fetchData(article_id) {
         let url = `https://sid-api-yrbb.onrender.com/articles/${article_id}`;
         let res = await apiFetch(url);
         let apiData = await res.json();
         setArticleData(apiData);
-        console.log(apiData);
     }
-
 
     return (
         <div className='article-text-content'>
-            {ArticleText(articleData)}
+            <ArticleText article_data={articleData} articleID={articleID} />
         </div>
     );
 }
 
-function ArticleText(article_data) {
-    if(!article_data) {
-        return <div></div>
+function ArticleText({ article_data, articleID }) {
+    const router = useRouter();
+
+    if (!article_data) {
+        return <div></div>;
     }
 
     const redirectToEdit = () => {
         router.push(`/Content/EditArticle?id=${articleID}`);
-      }
+    };
 
-    const title = article_data['title']
-    const section = article_data['section']
-    const content = article_data['content']
-    const updated_at = article_data['updated_at']
-    
+    const redirectToVersions = () => {
+        router.push(`/Content/ArticleVersions?id=${articleID}`);
+    };
+
+    const { title, section, content, updated_at } = article_data;
+
     return (
         <div className='article-text-div'>
-
             <p className='article-title'>{title}</p>
             <hr className="custom-line" />
-
             <p className='article-section'>Seção: {section}</p>
-            <p>Útlima atualização: {updated_at}</p>
+            <p>Última atualização: {updated_at}</p>
 
-            <button className='button'  onClick={redirectToEdit}>Editar artigo</button>
-            
-            <QuillTextEditor
-                value={content}
-            />
+            <div className='button-group-view'>
+                <button className='edit-button' onClick={redirectToEdit}>
+                    Editar artigo
+                </button>
+                <button className='versions-button' onClick={redirectToVersions}>
+                    Versões
+                </button>
+            </div>
 
-            <button className='button'  onClick={redirectToEdit}>Editar artigo</button>
+            <div className='content'>
+            <div dangerouslySetInnerHTML={{ __html: content }} />
+            </div>
         </div>
     );
 }
 
-export default ViewArticle
+export default ViewArticle;
